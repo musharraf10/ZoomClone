34b3bda92e13ecb9366afce049271fa489e51364
musharraf10 <skmusharaf00@gmail.com>
2025-01-08 17:13:44 +0530

    Fixed Bugs

---
+++ frontend/webpack.config.js
@@ -0,0 +1,5 @@
+export const performance = {
+    maxAssetSize: 1000000, // Adjust max size (in bytes) for the asset size warning
+    maxEntrypointSize: 1000000, // Adjust max size for the entry point size warning
+};
+  
