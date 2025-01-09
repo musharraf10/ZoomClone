57b4f82ca1763624ea08539c9ff7b3f803907dde
musharraf10 <skmusharaf00@gmail.com>
2025-01-09 12:05:16 +0530

    Updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    open: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  base: '/',
+});
